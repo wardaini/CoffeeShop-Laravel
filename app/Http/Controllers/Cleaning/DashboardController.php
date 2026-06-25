04b39d6377bac595b92bfbabac83a0dfe1e5b9a1
wardatul a'ani <awardatul5@gmail.com>
2026-06-26 05:42:01 +0700

    make new controller

---
+++ app/Http/Controllers/Cleaning/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Cleaning;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/IT/ActivityLogController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\IT;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ActivityLogController extends Controller
+{
+    //
+}
